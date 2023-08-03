94fd4fe9f3d18a157658df25e7c71c8605556c7c
Nicolas Senger <118479731+NicolasSenger@users.noreply.github.com>
2023-08-03 17:19:16 -0300

    Add files via upload

---
+++ Login_Form.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Gestor
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.btnClose_Click = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar_Click = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuario.Location = new System.Drawing.Point(122, 219);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(189, 13);
+            this.txtUsuario.TabIndex = 1;
+            this.txtUsuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label.Location = new System.Drawing.Point(53, 222);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(43, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Usuário";
+            // 
+            // btnClose_Click
+            // 
+            this.btnClose_Click.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClose_Click.FlatAppearance.BorderSize = 0;
+            this.btnClose_Click.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose_Click.Location = new System.Drawing.Point(188, 356);
+            this.btnClose_Click.Name = "btnClose_Click";
+            this.btnClose_Click.Size = new System.Drawing.Size(116, 58);
+            this.btnClose_Click.TabIndex = 3;
+            this.btnClose_Click.Text = "Cancelar";
+            this.btnClose_Click.UseVisualStyleBackColor = false;
+            this.btnClose_Click.Click += new System.EventHandler(this.btnClose_click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.Location = new System.Drawing.Point(122, 278);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(189, 13);
+            this.txtSenha.TabIndex = 5;
+            // 
+            // btnEntrar_Click
+            // 
+            this.btnEntrar_Click.BackColor = System.Drawing.Color.LightGreen;
+            this.btnEntrar_Click.FlatAppearance.BorderSize = 0;
+            this.btnEntrar_Click.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar_Click.Location = new System.Drawing.Point(45, 356);
+            this.btnEntrar_Click.Name = "btnEntrar_Click";
+            this.btnEntrar_Click.Size = new System.Drawing.Size(116, 58);
+            this.btnEntrar_Click.TabIndex = 6;
+            this.btnEntrar_Click.Text = "Entrar";
+            this.btnEntrar_Click.UseVisualStyleBackColor = false;
+            this.btnEntrar_Click.Click += new System.EventHandler(this.btnEntrar_click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "SGA - Sistema Gestor de Alunos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(132, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(135, 124);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(372, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEntrar_Click);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose_Click);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtUsuario);
+            this.MaximumSize = new System.Drawing.Size(388, 489);
+            this.MinimumSize = new System.Drawing.Size(388, 489);
+            this.Name = "Login_Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btnClose_Click;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnEntrar_Click;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Login_Form.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gestor
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            // Coloca a imagem user.png na picture box da janela
+            pictureBox2.Image = Image.FromFile("../../imagens/user.png");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEntrar_click(object sender, EventArgs e)
+        {
+            MEU_BD bancoDeDados = new MEU_BD();
+
+            MySqlDataAdapter adaptador = new MySqlDataAdapter();
+            DataTable tabela = new DataTable();
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM `usuario` WHERE `username` = @usn And `senha` =  @psw", bancoDeDados.getConexao);
+            // atualizando os valores do comando acima
+            comando.Parameters.Add("@usn", MySqlDbType.VarChar).Value = txtUsuario.Text;
+            comando.Parameters.Add("@psw", MySqlDbType.VarChar).Value = txtSenha.Text;
+
+            adaptador.SelectCommand = comando;
+
+            adaptador.Fill(tabela);
+
+            if (tabela.Rows.Count > 0)
+            {
+                this
+            }
+            else 
+            {
+                MessageBox.Show("Usuário ou senha  inválidos", "Erro de login", MessageBoxButtons.OK, MessageBoxIcon.Error);    
+            }
+        }
+    }
+}
